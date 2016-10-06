fa4bba34a5126556e1f0bbb56168cdba9c90e6c5
Brice Copy <brice.copy@gmail.com>
2016-10-06 22:26:48 +0200

    using cmcrobotics branch

--- modules/builder/script.sh
+++ modules/builder/script.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y --no-install-recommends wget pv apt-utils apt-transport-https
-apt-get install -y --no-install-recommends python python-dev python-pip python-virtualenv
+apt-get install -y --no-install-recommends build-essential i2c-tools python python-dev python-pip python-virtualenv python-smbus
 wget -q https://packagecloud.io/gpg.key -O - | sudo apt-key add -
 echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ jessie main' | sudo tee /etc/apt/sources.list.d/hypriot.list
 apt-get update
@@ -8,4 +8,4 @@ apt-get install -y docker-hypriot
 
 #  (!) the container needs to be run as PRIVILEGED to mount shared memory
 mvn -Dmaven.javadoc.failOnError=false install
-git clone https://github.com/nitinverma/maven-python-mojos.git
+git clone https://github.com/cmcrobotics/maven-python-mojos.git
