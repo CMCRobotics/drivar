9c306ab2641a157324d73ed4fe8e1d1ba36811cd
Brice Copy <brice.copy@cern.ch>
2018-05-21 20:56:52 +0200

    Add Brython-based simulator shell

---
+++ modules/examples/modules/brython-threejs/src/main/java/com/github/drivar/examples/brythonthreejs/BrythonThreejsApplication.java
@@ -0,0 +1,12 @@
+package com.github.drivar.examples.brythonthreejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrythonThreejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrythonThreejsApplication.class, args);
+	}
+}
---
+++ modules/examples/modules/brython-threejs/src/test/java/com/github/drivar/examples/brythonthreejs/BrythonThreejsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.drivar.examples.brythonthreejs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrythonThreejsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
